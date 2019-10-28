723c20088a90eb79e7816e7500261ca0f2419971
CicloM <CicloM@192.168.2.137>
2019-10-28 18:37:05 +0100

    :tada: Commit Inicial

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -15,15 +15,43 @@
  */
 package org.japo.java.main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author Bianca Antonela Glavan - biancaantonela.glavan.alum@iescamp.es
  */
 public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
     public static void main(String[] args) {
-        
-    
-        
+
+        double importeCena;
+        int numeroComensales;
+
+        try {
+            System.out.print("Importe cantidad cena: ");
+            importeCena = SCN.nextDouble();
+
+            System.out.print("Introduce el numero de comensales: ");
+            numeroComensales = SCN.nextInt();
+
+            double total = importeCena / numeroComensales;
+
+            System.out.println("Importe cena (€)..: " + importeCena);
+            System.out.println("Número comensales.: " + numeroComensales);
+            System.out.printf(Locale.ENGLISH, "Pago por Comensal.: %.2f€%n ", total);
+
+        } catch (Exception e) {
+            System.out.printf("ERROR: Entrada incorrecta ");
+        } finally {
+            SCN.nextLine();
+        }
+
     }
-    
+
 }
